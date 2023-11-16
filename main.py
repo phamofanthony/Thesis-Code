9c3ea063b6a3ac2d10862a962b528a7a2a39ea9c
phamofanthony <80490069+phamofanthony@users.noreply.github.com>
2023-11-15 23:36:18 -0600

    added pre-defined patterns

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 import json
 from collections import Counter
-from loggenerator import generate_data
+from loggenerator import *
 
 def main():
-    #create training data
-    generate_data(file_name="training_data", num_data_items=8, num_transactions=10000)
-    
-    #find common patterns
-    patterns = find_patterns(min_sequence_length=3, min_num_occurences=3)
+    #create training data / list of patterns
+    patterns = generate_patterns(num_data_items=8, num_transactions=5)
 
     #generate test data
-    generate_data(file_name="test_data", num_data_items=8, num_transactions=100)
+    generate_testing_data("testing_data", num_data_items=8, num_transactions=100, patterns_list=patterns)
 
     #analyze test data
     analyze_test_data(patterns)
 
 
-
-
-def find_patterns(min_sequence_length, min_num_occurences):
-    with open('training_data.json', 'r') as json_file:
-        data = json.load(json_file)
-    ops_arrays = [entry['ops'] for entry in data]
-    all_ops = [op for ops_array in ops_arrays for op in ops_array]
-    sequences = [tuple(all_ops[i:i+min_sequence_length]) for i in range(len(all_ops)-(min_sequence_length-1))] #Find sequences of length 3 or longer
-    sequence_counts = Counter(sequences) #Count the occurrences of each sequence
-    filtered_sequences = {seq: count for seq, count in sequence_counts.items() if count >= min_num_occurences} #Filter sequences that occur in 5 or more arrays
-    filtered_sequences_arrays = [list(seq) for seq, count in filtered_sequences.items()]
-    """ print("Sequences of length 3 or longer that occur in 3 or more arrays:")
-    for seq, count in filtered_sequences.items():
-        print(f"Sequence: {seq}, Occurrences: {count}")  """
-    return filtered_sequences_arrays
-
 def analyze_test_data(patterns):
-    with open('test_data.json', 'r') as json_file:
+    with open('testing_data.json', 'r') as json_file:
         objects = json.load(json_file)
     
     ops_arrays = [obj['ops'] for obj in objects]
